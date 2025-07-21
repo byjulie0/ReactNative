bb1155f525d1ba3ea97820c72607f1ecec0ebc0b
byjulie0 <julievieiramartins0@gmail.com>
2025-07-21 17:13:09 -0400

    Update index.tsx

--- app/index.tsx
+++ app/index.tsx
@@ -1,9 +1,25 @@
-import { Text, View } from "react-native";
+import { View, Text,Image} from "react-native";
+import { MaterialIcons } from "@expo/vector-icons";
+import AntDesign from '@expo/vector-icons/AntDesign';
 
 export default function Index() {
   return (
-    <View className="flex-1 justify-center items-center">
-      <Text className="text-5xl text-dark-200 font-bold">Bem vindo</Text>
+    <View className="flex-1 bg-gray-100">
+      <View className="flex-row justify-between items-start pt-4 px-4">
+        <AntDesign name="left" size={24} color="black" />
+        <View className="flex-row">
+          <MaterialIcons name="confirmation-number" size={24} color="#f24690" className="ml-10" />
+          <MaterialIcons name="shopping-cart" size={24} color="#f24690" className="ml-10" />
+        </View>
+      </View>
+      <View className="flex-row justify-between items-start ">
+        <Text className="font-bold text-2xl ml-8 ">Best Dress Collection</Text>
+        <Text className=" font-light text-base mr-3">8250 itens</Text>
+      </View>
+      <View className="flex-row justify-between items-start bg-white ml-8">
+        <Text>Category</Text>
+
+      </View>
     </View>
   );
 }
