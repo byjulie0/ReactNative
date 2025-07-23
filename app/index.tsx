cfcdcdc0bead56865ef77e37e7eada0c7e8f0873
byjulie0 <julievieiramartins0@gmail.com>
2025-07-23 16:31:31 -0400

    Update index.tsx

--- app/index.tsx
+++ app/index.tsx
@@ -1,24 +1,42 @@
-import { View, Text,Image} from "react-native";
+import { View, Text,Imagem } from "react-native";
 import { MaterialIcons } from "@expo/vector-icons";
 import AntDesign from '@expo/vector-icons/AntDesign';
 
 export default function Index() {
   return (
-    <View className="flex-1 bg-gray-100">
-      <View className="flex-row justify-between items-start pt-4 px-4">
+    <View className="flex-1 bg-gray-100 p-4">
+      <View className="flex-row justify-between items-center mb-4">
         <AntDesign name="left" size={24} color="black" />
-        <View className="flex-row">
-          <MaterialIcons name="confirmation-number" size={24} color="#f24690" className="ml-10" />
-          <MaterialIcons name="shopping-cart" size={24} color="#f24690" className="ml-10" />
+        <View className="flex-row gap-6">
+          <MaterialIcons name="confirmation-number" size={24} color="#f24690" />
+          <MaterialIcons name="shopping-cart" size={24} color="#f24690" />
         </View>
       </View>
-      <View className="flex-row justify-between items-start ">
-        <Text className="font-bold text-2xl ml-8 ">Best Dress Collection</Text>
-        <Text className=" font-light text-base mr-3">8250 itens</Text>
+      <View className=" flex-row justify-between">
+        <Text className="font-bold text-lg">Best Dress Collection</Text>
+        <Text className="font-light text-xs pr-2">8250 items</Text>
       </View>
-      <View className="flex-row justify-between items-start bg-white ml-8">
-        <Text>Category</Text>
 
+      <View className="flex-row flex-wrap items-center gap-2 bg-white p-2 rounded-2xl mt-2">
+        <Text className="flex-row items-center bg-gray-200 px-2 py-1 rounded-xl text-xs">
+          Category <AntDesign name="down" size={12} color="black" />
+        </Text>
+        <Text className="flex-row items-center bg-gray-200 px-2 py-1 rounded-xl text-xs">
+          Color <AntDesign name="down" size={12} color="black" />
+        </Text>
+        <Text className="flex-row items-center bg-gray-200 px-2 py-1 rounded-xl text-xs">
+          Size <AntDesign name="down" size={12} color="black" />
+        </Text>
+        <Text className="flex-row items-center bg-gray-200 px-2 py-1 rounded-xl text-xs">
+          Price <AntDesign name="down" size={12} color="black" />
+        </Text>
+        <Text className="flex-row items-center bg-gray-200 px-2 py-1 rounded-xl text-xs">
+          More <AntDesign name="down" size={12} color="black" />
+        </Text>
+      </View>
+      <View className="max-w-10 max-h-10 bg-white">
+        <Imagem></Imagem>
+        
       </View>
     </View>
   );
